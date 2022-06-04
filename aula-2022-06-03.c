e1967016ed0a580bb1724f86ded6a0cdca701ffc
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-06-03 21:16:50 -0300

    Create aula-2022-06-03.c

---
+++ aula-2022-06-03.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main() {
+
+    float saldo, soma, retirada, total;
+    
+    printf("me informe o valor da segunda-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
+
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &total)
+    printf("Agoa me informe o valor da retirada:\n");
+    scanf("%f", &total);
+    
+    soma = saldo - total;
+
+    printf("Valor do caixa na segunda feira; %f\n", soma);
+
+    if(saldo > 2000){
+    printf("Valor total de dinheiro recebido:%f\n",  );
+    printf("Valor total de pagamentos realizados:%f\n", );
+    printf("Qual é o saldo positivo atual:%f\n", );   
+}
+    else if(saldo < 500){
+    printf("Valor total de dinheiro recebido:%f\n", );
+    printf("Valor total de pagamentos realizados:%f\n", );
+    printf("Qual é o saldo positivo atual:%f\n", );
+}
+    else{
+}
+    return 0;
+}
