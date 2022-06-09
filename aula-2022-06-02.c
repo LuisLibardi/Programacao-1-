98d122de7b2f2f0948e97d6b87282b7ae9730b42
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-06-08 22:40:45 -0300

    Update aula-2022-06-02.c

--- aula-2022-06-02.c
+++ aula-2022-06-02.c
@@ -1,31 +1,155 @@
 #include <stdio.h>
  
-int main() {
+int main(){
 
-    float valor1, valor2, valor3;
-    float saldo, retirada;
+    float saldo, soma, retirada, total;
     
-    printf("me informe a entrada:\n");
-    scanf("%f", &valor1);
+    printf("Me informe o valor que entrou na segunda-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
 
-    printf("Agoa me informe o valor da retirada:\n");
-    scanf("%f", &valor2);
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &retirada);
 
-    saldo = (valor1 + valor2) - valor2;
+    saldo = retirada + total;
+    soma = saldo - total;
+    total = retirada + saldo;
+
+    printf("Valor do caixa na segunda feira; %f\n", total);
 
     if(saldo > 2000){
-    printf("Valor total de dinheiro recebido:%f\n", valor1);
-    printf("Valor total de pagamentos realizados:%f\n", valor2);
+    printf("Valor total de dinheiro recebido:%f\n",  saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);   
+}
+    else if(saldo < 500){
+    printf("Valor total de dinheiro recebido:%f\n", saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
     printf("Qual é o saldo positivo atual:%f\n", saldo);
+}
     
+//*
+
+    printf("Me informe o valor que entrou na terca-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
+
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &total);
+
+     saldo = soma + total;
+
+    printf("Agoa me informe o valor que foi retirado:\n");
+    scanf("%f", &total);
+    
+    soma = saldo - total;
+
+    printf("Valor do caixa na terca-feira; %f\n", soma);
+
+    if(saldo > 2000){
+    printf("Valor total de dinheiro recebido:%f\n",  saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);   
+}
+    else if(saldo < 500){
+    printf("Valor total de dinheiro recebido:%f\n", saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);
 }
 
+//*
+    
+    printf("Me informe o valor que entrou na quarta-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
+
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &total);
+
+     saldo = soma + total;
+
+    printf("Agoa me informe o valor que foi retirado:\n");
+    scanf("%f", &total);
+    
+    soma = saldo - total;
+
+    printf("Valor do caixa na quarta-feira; %f\n", soma);
+
+    if(saldo > 2000){
+    printf("Valor total de dinheiro recebido:%f\n",  saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);   
+}
     else if(saldo < 500){
-    printf("Valor total de dinheiro recebido:%f\n", valor1);
-    printf("Valor total de pagamentos realizados:%f\n", valor2);
+    printf("Valor total de dinheiro recebido:%f\n", saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
     printf("Qual é o saldo positivo atual:%f\n", saldo);
 }
-    else{
+
+//*
+
+ printf("Me informe o valor que entrou na quinta-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
+
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &total);
+
+     saldo = soma + total;
+
+    printf("Agoa me informe o valor que foi retirado:\n");
+    scanf("%f", &total);
+    
+    soma = saldo - total;
+
+    printf("Valor do caixa na quinta-feira; %f\n", soma);
+
+    if(saldo > 2000){
+    printf("Valor total de dinheiro recebido:%f\n",  saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);   
 }
+    else if(saldo < 500){
+    printf("Valor total de dinheiro recebido:%f\n", saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);
+}
+
+//*
+
+ printf("Me informe o valor que entrou na sexta-feira:\n");
+    scanf("%f", &total);
+    
+    saldo = saldo + total;
+
+    printf("Qual foi o valor pago neste dia:");
+    scanf("%f", &total);
+
+     saldo = soma + total;
+
+    printf("Agoa me informe o valor que foi retirado:\n");
+    scanf("%f", &total);
+    
+    soma = saldo - total;
+
+    printf("Valor do caixa na sexta-feira; %f\n", soma);
+
+    if(saldo > 2000){
+    printf("Valor total de dinheiro recebido:%f\n",  saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);   
+}
+    else if(saldo < 500){
+    printf("Valor total de dinheiro recebido:%f\n", saldo);
+    printf("Valor total de pagamentos realizados:%f\n", soma);
+    printf("Qual é o saldo positivo atual:%f\n", saldo);
+}
+
+printf("Fim");
+
     return 0;
 }
