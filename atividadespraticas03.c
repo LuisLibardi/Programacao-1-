71b750ebcde36be993e7d59b49a57c17355f7d31
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-07-08 21:44:27 -0300

    Create atividadespraticas03.c

---
+++ atividadespraticas03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+int vet[15], i;
+	
+	for (i = 0; i <= 15 ; i++) {
+	
+	vet[i]= 2 * i + 1;
+	
+	printf("valores do vetor [%d]: %d\n", i, vet[i]);
+ 			 		
+	}
+}
