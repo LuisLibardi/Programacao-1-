0b540f175711187f975f0866e6d14e2844df5a27
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-07-14 19:00:29 -0300

    Create aula-2022-07-14.c

---
+++ aula-2022-07-14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int num, flag, i, vet[50], pos;
+	for (i = 0; i < 50; i++)
+		vet[i] = rand()%100;
+		
+	for ( pos = 0; pos < 50; pos++)
+	{
+		flag = 0;
+
+		for (i = 2; i < num-1 ; i++){
+			if(vet[pos] % i == 0)
+				flag = 1;
+		}
+		if (flag == 0)
+			printf("pos %d: %d é primo\n", pos, vet[pos]);
+	}
+}
+	
