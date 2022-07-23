916288382c6bea50d2ad3a3fe2b4047d9088ff85
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-07-22 21:39:09 -0300

    Create 22-2022-07.c

---
+++ 22-2022-07.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct{
+	char nome[20];
+	int idade;
+	float nota;
+} tipo_aluno;
+
+int i;
+
+int main(){
+
+tipo_aluno aluno[3];
+
+for(i = 0; i < 3; i++)
+{
+	printf("nome do aluno %d:\n", i);
+	scanf("%s", aluno[i].nome);
+	printf("Idade do aluno:\n");
+	scanf("%d", &aluno[i].idade);
+	printf("Nota do aluno:\n");
+	scanf("%f", &aluno[i].nota);
+}	
+for(i = 0; i < 3; i++)
+	printf("Nome:%s idade:%d nota:%.2f\n", aluno[i].nome, aluno[i].idade, aluno[i].nota);
+	
+}
