0a119ae29bcca93f90b8f231846e93faa35b08ce
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-08-04 19:15:43 -0300

    Update 22-2022-07.c

--- 22-2022-07.c
+++ 22-2022-07.c
@@ -25,3 +25,25 @@ for(i = 0; i < 3; i++)
 	printf("Nome:%s idade:%d nota:%.2f\n", aluno[i].nome, aluno[i].idade, aluno[i].nota);
 	
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int *n1, *n2, *soma;
+	
+	n1 = malloc(sizeof(int));
+	n2 = malloc(sizeof(int));
+	soma = malloc(sizeof(int));
+	
+	printf("Digite n1:");
+	scanf("%d", n1);
+	
+	printf("Digite n2:");
+	scanf("%d", n1);
+	
+	*soma = *n1 + *n2;
+	
+	printf("Valor da soma: %d", *soma);
+
+}
