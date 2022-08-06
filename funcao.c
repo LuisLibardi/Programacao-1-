807fc196f9764a9c52538a572ece498c3e02f6ab
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-08-05 21:19:35 -0300

    Create funcao.c

---
+++ funcao.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct{
+	float peso, altura;
+}tipoIMC;
+
+void calc_imc(TipoIMC*imc){	
+	float resultado;
+	resultado = imc -> peso/pow(imc -> altura, 2);
+	printf("%f\n", resultado);
+}
+
+int main(){
+	tipoIMC*imc;
+	imc = malloc(sizeof(tipoIMC));
+	
+	printf("Qual o seu peso?\n");
+	scanf("%f", &imc -> peso);
+	
+	printf("Me informe sua altura?\n");
+	scanf("%f", &imc -> altura);
+	
+	printf("imc: %f\n",imc -> peso/pow (imc -> altura, 2));
+}	
+
+
+
